23c2314a055827f793648287c5d62757acf95c6e
Yujian <yujian_fu@outlook.com>
2020-05-25 05:33:38 +0800

    add test results

--- examples/run_sift1b.sh
+++ examples/run_sift1b.sh
@@ -43,7 +43,7 @@ opq="off"             # Turn on/off opq encoding
 #######################################
 
 k="100"                 # Number of the closest vertices to search
-nprobe="64"           # Number of probes at query time
+nprobe="100"           # Number of probes at query time
 max_codes="30000"     # Max number of codes to visit to do a query
 efSearch="100"        # Max number of candidate vertices in priority queue to observe during searching
 
