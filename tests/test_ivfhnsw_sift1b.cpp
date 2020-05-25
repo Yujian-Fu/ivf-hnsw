d15177b8d1bd9dab55fe38bfa152f01b4a162386
Yujian <yujian_fu@outlook.com>
2020-05-25 08:30:39 +0800

    add test results

--- tests/test_ivfhnsw_sift1b.cpp
+++ tests/test_ivfhnsw_sift1b.cpp
@@ -182,8 +182,8 @@ int main(int argc, char **argv)
 
         //5, 10, 20, 30, 30, 40, 50, 60, 60, 70, 80, 80, 90, 100, 100, 
         //10000, 10000, 10000, 10000, 20000, 20000, 20000, 30000, 40000, 40000, 50000, 60000, 60000, 70000, 80000, 
-        int nprobe_list[5] = {90, 120, 130, 140, 150};
-        int max_codes_list[5] = {70000, 80000, 90000, 100000, 100000};
+        int nprobe_list[5] = {90, 120, 120, 140, 150};
+        int max_codes_list[5] = {70000, 70000, 80000, 100000, 100000};
         for (int para = 0; para < 5; para++){
             opt.nprobe = nprobe_list[para];
             opt.max_codes = max_codes_list[para];
