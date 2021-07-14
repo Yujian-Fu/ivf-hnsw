18a98a39ab08167fa15bee49cfbbdfac01c0faf4
Yujian <yujian_fu@outlook.com>
2021-07-15 02:08:38 +0800

    final results

--- examples/run_sift1b_grouping_OPQ.sh
+++ examples/run_sift1b_grouping_OPQ.sh
@@ -47,9 +47,9 @@ opq="on"              # Turn on/off opq encoding
 #######################################
 
 k="1"                 # Number of the closest vertices to search
-nprobe="100"           # Number of probes at query time
+nprobe="110"           # Number of probes at query time
 max_codes="200000"     # Max number of codes to visit to do a query
-efSearch="100"         # Max number of candidate vertices in priority queue to observe during seaching
+efSearch="110"         # Max number of candidate vertices in priority queue to observe during seaching
 pruning="on"          # Turn on/off pruning
 
 #########
